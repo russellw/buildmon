2632a8526373a5611d9ac4852121506ccf1d36d8
Russell Wallace <russell.wallace@gmail.com>
2015-06-28 02:55:24 +0100

    refactor

--- buildmon.cpp
+++ buildmon.cpp
@@ -9,8 +9,8 @@
 
 static void err(char *s) {
   // Retrieve the system error message for the last-error code
-  char *msg;
   auto e = GetLastError();
+  char *msg;
   FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
                     FORMAT_MESSAGE_IGNORE_INSERTS,
                 0, e, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char *)&msg,
@@ -42,13 +42,13 @@ int main(int argc, char **argv) {
   auto properties = (EVENT_TRACE_PROPERTIES *)malloc(BufferSize);
 
   ZeroMemory(properties, BufferSize);
-  properties->Wnode.BufferSize = BufferSize;
-  properties->Wnode.Flags = WNODE_FLAG_TRACED_GUID;
-  properties->Wnode.ClientContext = 3;
-  properties->Wnode.Guid = SystemTraceControlGuid;
   properties->EnableFlags = EVENT_TRACE_FLAG_NETWORK_TCPIP;
   properties->LogFileMode = EVENT_TRACE_REAL_TIME_MODE;
   properties->LoggerNameOffset = sizeof(EVENT_TRACE_PROPERTIES);
+  properties->Wnode.BufferSize = BufferSize;
+  properties->Wnode.ClientContext = 3;
+  properties->Wnode.Flags = WNODE_FLAG_TRACED_GUID;
+  properties->Wnode.Guid = SystemTraceControlGuid;
   strcpy((char *)properties + properties->LoggerNameOffset, KERNEL_LOGGER_NAME);
 
   ControlTrace(0, KERNEL_LOGGER_NAME, properties, EVENT_TRACE_CONTROL_STOP);
@@ -58,13 +58,13 @@ int main(int argc, char **argv) {
   if (ERROR_SUCCESS != status)
     err("StartTrace");
 
-  EVENT_TRACE_LOGFILE LogFile = {0};
-  LogFile.LoggerName = KERNEL_LOGGER_NAME;
-  LogFile.ProcessTraceMode =
+  EVENT_TRACE_LOGFILE log = {0};
+  log.EventRecordCallback = EventRecordCallback;
+  log.LoggerName = KERNEL_LOGGER_NAME;
+  log.ProcessTraceMode =
       (PROCESS_TRACE_MODE_REAL_TIME | PROCESS_TRACE_MODE_EVENT_RECORD |
        PROCESS_TRACE_MODE_RAW_TIMESTAMP);
-  LogFile.EventRecordCallback = EventRecordCallback;
-  trace = OpenTrace(&LogFile);
+  trace = OpenTrace(&log);
   CreateThread(0, 0, processThread, 0, 0, 0);
 
   wprintf(L"Press any key to end trace session ");
